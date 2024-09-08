9bdea574607e77f0ec7582c5b128ba325ef9fb8d
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-08 15:49:53 -0400

    Create determinePreformanceRating function

--- sales_preformance_analyzer.js
+++ sales_preformance_analyzer.js
@@ -9,6 +9,18 @@ return averageSales;
 }
 console.log(calculateAverageSales(salesFiguresMaddy)); // outputs 23.333
 
-let salesFiguresFrank = [10,20,30,50,60] //re-testing function
+let salesFiguresFrank = [20.00,30.00,40.00,50.00,60.00] //re-testing function
 console.log(calculateAverageSales(salesFiguresFrank)); //outputs 34
 
+//Determine Preformance Rating
+
+function determinePreformanceRating(averageSales){
+    let preformanceRating;
+    if (averageSales > 10000.00 ) return "Excellent";
+    else if (averageSales >= 7000.00 && averageSales <= 10000.00) return "Good";
+    else if (averageSales >= 4000.00 && averageSales <= 70000.00) return "Satisfactory";
+    else if (averageSales < 4000.00)  return "Needs Improvement";
+
+}
+let averageSalesFrank = (calculateAverageSales(salesFiguresFrank)); //define averageSales for frank
+console.log("Preformace Rating:", determinePreformanceRating(averageSalesFrank)); //outputs Needs improvement
